5e3d957e3210d18d4b8b6d9f2f796918b82190c8
Saad Rassam <smrassam@gmail.com>
2020-08-11 23:11:37 -0400

    Final product

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,20 @@ var currentHour = 12
 var currentHour = moment().format("H")
 var currentHour = 12
 
-for (let i - 9; i < 18; i++) {
+for (let i = 9; i < 18; i++) {
     if (currentHour < i) {
         $("#hour-" + i).addClass("future")
     }
     else if (parseInt(currentHour) === i) {
         $("#hour-" + i).addClass("present")
     }
+    var localStore = localStorage.getItme("hour-" + i)
+    if (localStore !== null) {
+        $("#hour-" + i).text(localStore)
+    }
 }
+$(".saveBtn").on("click", function () {
+    var clickHour = $(this).val()
+    var textAreaContent = $("#hour-" + clickedHour).val()
+    localStorage.setItem("hour-" + clickedHour, textAreaContent)
+})
