4f43f06e8c2fdfdd9bf369f24d1fe373b32f36c3
Saad Rassam <smrassam@gmail.com>
2020-08-11 22:59:13 -0400

    updates to js code

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 $("#date").text(moment().format("MMM Do YYYY"))
+
+var currentHour = 12
+
+var currentHour = moment().format("H")
+var currentHour = 12
+
+for (let i - 9; i < 18; i++) {
+    if (currentHour < i) {
+        $("#hour-" + i).addClass("future")
+    }
+    else if (parseInt(currentHour) === i) {
+        $("#hour-" + i).addClass("present")
+    }
+}
